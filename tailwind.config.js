bf759e596a4dc4361392ad0ab564e45dc84e40e0
JasperVijayan <141350291+JasperVijayan@users.noreply.github.com>
2023-08-03 21:53:05 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {border: {
+      'b-1': '1px solid',
+    },},
+  },
+  plugins: [],
+}
+
